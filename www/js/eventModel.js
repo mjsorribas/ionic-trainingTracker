afb7476f26a0131d6edd4c44a548fc6c7869eec0
Magnus Osbeck <magnus.osbeck@lineducation.se>
2016-02-10 00:52:09 +0100

    updated the activity list

--- www/js/eventModel.js
+++ www/js/eventModel.js
@@ -22,18 +22,9 @@ eventModule.factory('storage', ['localStorageService', '$mixpanel', function (lo
 
   var self = this;
   self.presetActivies = {
-    'testevent': {
-      id: 'testevent',
-      name: 'Monsterevent som testar',
-      description: 'testevent testevent testevent testevent testevent testevent testevent testevent testevent testevent.',
-      active: false,
-      count: 0,
-      timeLimit: 10,
-      currentEvent: undefined
-    },
     'eatingday': {
       id: 'eatingday',
-      name: 'Äntligen ätardag',
+      name: 'Eating what ever, day!',
       description: 'Fördelen jag ser av att ha en ätardag är att jag blir mer motiverad av att äta bra under 6 dagar innan jag får äta något gott på dag 7. Då har jag alltid något att se fram emot och när jag väl äter den onyttiga maten eller godiset kommer jag med största sannolikhet att äta betydligt mindre av det än om jag hade ätit det oftare.',
       active: false,
       count: 0,
@@ -42,7 +33,7 @@ eventModule.factory('storage', ['localStorageService', '$mixpanel', function (lo
     },
     'back': {
       id: 'back',
-      name: 'Dead back exercise',
+      name: 'Back breaker!',
       description: 'Starka ryggmuskler och god rörlighet i ryggraden förebygger ryggont och förbättrar din hållning. Det är viktigt att träna både styrka och rörlighet för ryggen.',
       active: false,
       count: 0,
@@ -51,7 +42,7 @@ eventModule.factory('storage', ['localStorageService', '$mixpanel', function (lo
     },
     'chest': {
       id: 'chest',
-      name: 'Compressd tiddies',
+      name: 'Chest pump!',
       description: ' Det är bättre att köra med lite lättare vikter och ha kontroll genom övningen, än att köra för tungt. Träna igenom hela bröstet, var otroligt noga med tekniken och höj successivt vikterna. Då kommer också resultaten.',
       active: false,
       count: 0,
@@ -60,7 +51,7 @@ eventModule.factory('storage', ['localStorageService', '$mixpanel', function (lo
     },
     'shoulders': {
       id: 'shoulders',
-      name: 'Axlar',
+      name: 'Burning shoulders!',
       description: 'Att värma upp axlarna är oerhört viktigt men framförallt är det rotatorcuffen vi vill ska bli ordentligt uppvärmd. I din axel sitter en kulled som hålls på plats i en sockel av 4 små muskler.',
       active: false,
       count: 0,
@@ -69,7 +60,7 @@ eventModule.factory('storage', ['localStorageService', '$mixpanel', function (lo
     },
     'arms': {
       id: 'arms',
-      name: 'Armar',
+      name: 'Iron arms!',
       description: 'Börja med att göra övningarna två gånger i veckan och ha som mål att öka till tre. Du kan välja att göra tre av övningarna, en för varje muskel, ena gången och de andra tre nästa gång. Gör så många repetitioner du orkar och sedan några till. Set och siffror är inte så viktigt, det viktiga är att du gör det. På bara tre veckor ser du skillnad.',
       active: false,
       count: 0,
@@ -78,7 +69,7 @@ eventModule.factory('storage', ['localStorageService', '$mixpanel', function (lo
     },
     'legs': {
       id: 'legs',
-      name: 'Ben',
+      name: 'Power legs!',
       description: 'När du tränar dina ben så kommer produktionen av tillväxthormon och testosteron öka markant. Detta bidrar till ökad tillväxt på övriga delar av kroppen också. Därför är det viktigt att du regelbundet styrketränar dina ben. Även om det inte gillas så kommer din kropp belöna dig med en snabb viktuppgång.',
       active: false,
       count: 0,
@@ -87,7 +78,7 @@ eventModule.factory('storage', ['localStorageService', '$mixpanel', function (lo
     },
     'cardio': {
       id: 'cardio',
-      name: 'Konditiont... Usch!',
+      name: 'Sweaty cardio!',
       description: 'Basket uppfanns 1891 av den kanadensiske KFUM-tränaren James Naismith. Idag hör den till de mest utövade sporterna över hela världen och i USA är den av särskilt stor betydelse. Vart fjärde år spelas ett världsmästerskap i basket, arrangerat av internationella basketfederationen, Fédération Internationale de Basketball.',
       active: false,
       count: 0,
