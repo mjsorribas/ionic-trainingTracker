bfd25809904212fabd2e44fdaa5b44530a4584a4
Magnus Osbeck <magnus.osbeck@lineducation.se>
2016-02-22 13:23:09 +0100

    firebase structure

---
+++ www/js/dataBaseStructure.js
@@ -0,0 +1,41 @@
+var Users =
+{
+  "userUuid": {
+    "name": "Mary Chen",
+    "groups": {
+      "groupUuid_1": true,
+      "groupUuid_2": true,
+      "groupUuid_3": true
+    },
+    firends : {
+      "userUuid_1": true,
+      "userUuid_2": true,
+      "userUuid_3": true
+    }
+  }
+};
+
+var groups =
+{
+  "groupUuid": {
+    "name": "Group Name",
+    "members": {
+      "userUuid_1": true,
+      "userUuid_2": true,
+      "userUuid_3": true
+    }
+  }
+};
+var Messages =
+{
+  "userUuid" : {
+    "messesUuid" : 'messesUuid',
+    "title": 'title',
+    "body" : 'body',
+    "groups" : {
+      "userUuid_1": true,
+      "userUuid_2": true,
+      "userUuid_3": true
+    }
+  }
+};
